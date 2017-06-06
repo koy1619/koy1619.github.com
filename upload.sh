677bc1ba2342f905f51a654519898feecba10040
koy1619 <maxiaolei007@sina.com>
2017-06-06 16:02:39 +0800

    Site updated: 2017-06-06-----16:02:39

--- upload.sh
+++ upload.sh
@@ -6,6 +6,6 @@ touch `date +%Y-%m-%d-----%H:%M:%S`
 
 git init
 git add -A
-git commit -m "update" .
+git commit -m "Site updated: `date +%Y-%m-%d-----%H:%M:%S`" .
 git remote set-url origin git@github.com:koy1619/koy1619.github.com.git
 git push -u origin master
